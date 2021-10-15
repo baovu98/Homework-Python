db24db7124ac20f0f6a256d8fbdc1dacbb60a1e7
Bao Vu <baovu@baos-mbp.lan>
2021-10-14 17:09:37 -0700

    Nani

--- multiply.py
+++ multiply.py
@@ -1,23 +1,19 @@
-def multiply_list(myList):
-
-	# Multiply elements one by one
+def multiply_list(Lalisalist):
+	# Multiply the Numbers in the list one by one
     result = 1
-    for i in myList:
+    for i in Lalisalist:
           try:
                int(i)
           except:
                return False
 
-    for i in myList:
+    for i in Lalisalist:
          result = result * i
          
     return result
 
-#input your willing number in the list here
-
+#Insert Desired numbers
 yourList = []
 yourList = list(map(int,input("Input: ").split()))
 print("Input: ",yourList)
-
-
 print("Output: ",multiply_list(yourList))
